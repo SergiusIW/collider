7df8239377fed16e8eca6c4f099f5e30db388f82
Matthew Michelotti <michelotti.matthew@gmail.com>
2016-01-31 01:25:25 -0600

    updated copyright notices

--- collider/src/main/java/com/matthewmichelotti/collider/EReiterate.java
+++ collider/src/main/java/com/matthewmichelotti/collider/EReiterate.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2014 Matthew D. Michelotti
+ * Copyright 2013-2016 Matthew D. Michelotti
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
